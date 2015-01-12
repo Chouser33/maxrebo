9d73ddc0cdaebe7532a718816b84992849021f73
Chouser33 <chouser@monetate.com>
2015-01-12 14:46:07 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.embedly',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
